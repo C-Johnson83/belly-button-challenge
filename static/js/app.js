6044f1526516ffa9497810764560e5f4671adaad
Chrispy83 <chrisj21283@gmail.com>
2023-04-02 21:44:27 -0400

    still can't change all plots on Option change, cleaned a little and added code descriptions. More colors!

--- static/js/app.js
+++ static/js/app.js
@@ -1,154 +1,159 @@
-
+// Start the page with default visualizations so it is not a blank page
 function init() {
-
-    // Get Data Set
-        
+    // Get the Data
     const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-        
     d3.json(url).then(function(data) {
-        data = data;
-        
+        console.log(`Initial Data`);
         console.log(data);
-        
+    // Get Names
     let names = Object.values(data.names);
-        console.log(names)
-
+        console.log(`Names`);
+        console.log(names);
     // Get Metadata
-
     let metadata = Object.values(data.metadata[0]);
-    console.log(`Metadata`)
-    console.log(metadata)
-
+        console.log(`Metadata`);
+        console.log(metadata);
     // Get Sample Values
-
     let sampVal = Object.values(data.samples[0].sample_values);
         console.log(`Sample Values`);
         console.log(sampVal);
-    
     // Get OTU ID's
-        
     let otuIDs = Object.values(data.samples[0].otu_ids);
         console.log(`OTU ID's`);
         console.log(otuIDs);
-    
-
-        let otuLabels = Object.values(data.samples[0].otu_labels);
+    // Get OTU Labels
+    let otuLabels = Object.values(data.samples[0].otu_labels);
         console.log(`OTU Labels`);
         console.log(otuLabels);
-  
-    
-// Populate the dropdown from the list/array
+    // Populate the dropdown from the list/array
     let selector = d3.select("#selDataset");
-    for (let i = 0; i < names.length; i++){
-    console.log(data[names[i]])
-    selector.append("option").text(names[i]).property("value", names[i]);
+        for (let i = 0; i < names.length; i++){
+            console.log(data[names[i]])
+            selector.append("option").text(names[i]).property("value", names[i]);
     }
-
+    // Populate the Demographic Info
     d3.select("#sample-metadata").html(displayMeta(metadata));
-
-    displayMeta(metadata)
-    displayBar(sampVal, otuIDs)
-    displayBubble(otuIDs, sampVal, otuLabels)
-    displayWashing(metadata)
-})};
-
+    displayMeta(metadata);
+    // Call the Bar Function defined below
+    displayBar(sampVal, otuIDs);
+    // Call the Bubble Function defined below
+    displayBubble(otuIDs, sampVal, otuLabels);
+    }
+    )
+};
+// Update the charts when the Test Subject ID is changed
 function optionChanged(){
+    // Get the Data
     const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-        
     d3.json(url).then(function(data) {
         let menu = d3.select("#selDataset");
         let dataset = menu.property("value");
         console.log(`user selected ${dataset}`);
-
-       
-        let newData = data.samples.find(sample => sample.id === dataset);
-        if (newData) {
-            let newSamp = newData.sample_values;
-            let newOtu = newData.otu_ids;
-            let newLabels = newData.otu_labels;
-            
-           
-            let x = newSamp.slice(0,10).reverse();
-            let y = newOtu.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
-            displayBar(newSamp, newOtu);
-            displayBubble(newOtu, newSamp, newLabels);
-        } 
+     // Make variables for the selected Test Subject Id
+    let newData = data.samples.find(sample => sample.id === dataset);
+    if (newData) {
+        let newSamp = newData.sample_values;
+        let newOtu = newData.otu_ids;
+        let newLabels = newData.otu_labels;
+        let newMetadata = data.metadata.find(meta => meta.id === dataset);
+        if (newMetadata) {
+            displayMeta[newMetadata];
+        }
+        // Set the Bar x and y values
+        let x = newSamp.slice(0,10).reverse();
+        let y = newOtu.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
+        // Re-Call Bar and Bubble Functions
+        displayBar(newSamp, newOtu);
+        displayBubble(newOtu, newSamp, newLabels);
+        }
     });
 }
-
+    ////////////////////////////////////////////
+   //    Collection of defined               //
+  //    Functions to plot the charts        //
+ //    and the Demographic Info            //
+////////////////////////////////////////////
+// Bar chart function
 function displayBar(sampVal, otuIDs) {
-    console.log("displaybar")
+    console.log(`displaybar`)
+    // Set the Bar x and y values
     let x = sampVal.slice(0,10).reverse();
     let y = otuIDs.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
-    console.log(x)
-    console.log(y)  
+    console.log(x);
+    console.log(y) ;
     let barChart = [{
         x: x,
         y: y,
         type: "bar",
         orientation: "h",
         marker : {
-            color: "#c242f5"
+            color: "cyan",
+            line : {
+            color:'Magenta',
+            width: 2
         }
-       
-    }];
-
+    }
+    }
+    ];
+    // Set the size of the chart visualization and Title
     let barLayout = [{
         title: "Top 10 Belly Bacts",
-
-
         margin: { t: 30, l: 150 },
-        paper_bgcolor: "lavender"
-    }];
-
-    Plotly.newPlot("bar", barChart, barLayout);
-
-  }
-
-function displayBubble(otuIDs, sampVal, otuLabels) {
-    
-    // Since the bubble chart displays all of the bacterium found in the belly button, 
-    
-        let a = otuIDs;
-        let b = sampVal;
-        let text = otuLabels;
-        let marker_size = sampVal;
-        let marker_color = otuIDs 
-    
-        console.log(text)
-    
-        let bubbleChart = [{
-            x: a,
-            y: b,
-            text: text,
-            mode: "markers",
-            marker: {
-                size: marker_size,
-                color: marker_color,
-                colorscale: 'Picnic', 
-            }
-        }];
-    
-        let bubbleLayout = [{
-            title: "Top 10 Belly Bacts",
-           
-            xaxis:{
-                title: "OTU-IDs"
-            },
-            showlegend: true,
-            paper_bgcolor: "lavender",
-            height: 700,
-            width: 1500,
-        }];
-    
-        Plotly.newPlot("bubble", bubbleChart, bubbleLayout);
     }
-
-    function displayMeta(metadata){let demo = "";
+    ];
+    // Plot the chart when function is called
+    Plotly.newPlot("bar", barChart, barLayout);
+}
+// Bubble chart function
+function displayBubble(otuIDs, sampVal, otuLabels) {
+    console.log(`Display Bubble`)
+    // Set the Bubble variables
+    let a = otuIDs;
+    let b = sampVal;
+    let text = otuLabels;
+    let marker_size = sampVal;
+    let marker_color = otuIDs
+   console.log(`OTU ID's`);
+   console.log(a);
+   console.log(`Sample Values`);
+   console.log(b);
+    let bubbleChart = [{
+        x: a,
+        y: b,
+        text: text,
+        mode: "markers",
+        marker: {
+            size: marker_size,
+            color: marker_color,
+            colorscale: 'Blues',
+            line: {
+                color: "magenta",
+                width: 2
+            }
+        }
+    }
+    ];
+    // Set the size, title, and other parameters of the chart visualization
+    let bubbleLayout = [{
+        title: "Top 10 Belly Bacts",
+        xaxis:{
+            title: "OTU-IDs"
+        },
+        showlegend: true,
+        paper_bgcolor: "lavender",
+        height: 700,
+        width: 1800,
+    }
+    ];
+    // Plot the chart when function is called
+        Plotly.newPlot("bubble", bubbleChart, bubbleLayout);
+}
+// Demographic Info function for the default visualization
+function displayMeta(metadata){let demo = "";
     Object.entries(metadata).forEach(([key, value]) => demo += `<br>${key}: ${value}<br>`)
     return demo;
-        }
-init()      
-   d3.selectAll("#selDataset").on("change", optionChanged);   
-
-        
+}
+// Finally, Call the actual initialization function set at the begining
+init()
+// Call the optionChanged function to update the plots when the ID is changed
+d3.selectAll("#selDataset").on("change", optionChanged);
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,54 +1,62 @@
-let newMetadata = data.metadata.find(meta => meta.id === dataset);
-if (newMetadata) {
-    displayWashing(newMetadata),
-    displayMeta(newMetadata);
-    } 
-
-
+// Get the Data
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+d3.json(url).then(function(data) {
+// Get Metadata
+let metadata = Object.values(data.metadata[0]);
+  displayWashing(metadata)
+});
+function metaChange(){
+      // Get the Data
+  const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+  d3.json(url).then(function(data) {
+      // Get Metadata
+  let menu = d3.select("#selDataset");
+  let dataset = menu.property("value");
+  let newMetadata = data.metadata.find(meta => meta.id === dataset);
+  if (newMetadata) {
+      displayWashing(newMetadata),
+      displayNewMeta(newMetadata);
+  }
+  }
+  )
+};
 function displayWashing(metadata) {
-    
-    let value = metadata[6];
-
+  let value = metadata[6];
 // Check to see if the data we're getting is the data we need
-    console.log(`Daily Washing`)
-    console.log(value)
-
-    let gaugeChart = [{
-        domain: { x: [0, 1], y: [0, 1] },
-        value: value,
-        type: "indicator",
-        axis: { range: [null, 7] },
-        title: { text: "Washing Frequency" },
-        mode: "gauge+number",
-        gauge: {
-            axis: { range: [null, 7], tickwidth: 1 },
-            bar: { color: "green", thickness: 0.1 },
-            steps: [
-                { range: [0, 2], color: "blue" },
-                { range: [2, 4], color: "purple" },
-                { range: [4, 7], color: "red" }
-            ],
-            threshold: {
-                line: { color: "red", width: 4 },
-                thickness: 0.75,
-                value: value
-              }
-        }
-    }];
-
-    let gaugeLayout = { 
-        width: 600, 
-        height: 500, 
-        margin: { t: 25, r: 25, l: 25, b: 25 },
-        paper_bgcolor: "lavender",
-        font: { color: "darkblue", family: "Arial" }
-    };
-
-    Plotly.newPlot("gauge", gaugeChart, gaugeLayout);
+  console.log(`Daily Washing`)
+  console.log(value)
+  let gaugeChart = [{
+      domain: { x: [0, 1], y: [0, 1] },
+      value: value,
+      type: "indicator",
+      axis: { range: [null, 7] },
+      title: { text: "Washing Frequency" },
+      mode: "gauge+number",
+      gauge: {
+          axis: { range: [0, 9], tickwidth: 1 },
+          bar: { color: "Magenta", thickness: 0.1 },
+          steps: [
+              { range: [0, 3], color: "blue" },
+              { range: [3, 6], color: "cyan" },
+              { range: [6, 9], color: "lightblue"}
+          ],
+          threshold: {
+              line: { color: "Magenta", width: 4 },
+              thickness: 0.75,
+              value: value
+            }
+      }
+  }];
+  let gaugeLayout = {
+      width: 600,
+      height: 500,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      // paper_bgcolor: "aquamarine",
+      font: { color: "Magenta", family: "Arial" }
+  };
+  Plotly.newPlot("gauge", gaugeChart, gaugeLayout);
 }
-
-
-function displayMeta(metadata){
-    let demo = "";
-    Object.entries(metadata).forEach(([key, value]) => demo += `<br>${key}: ${value}<br>`)
-    return demo;}
+function displayNewMeta(newMetadata){
+  let demo = "";
+  Object.entries(newMetadata).forEach(([key, value]) => demo += `<br>${key}: ${value}<br>`)
+  return demo;}
